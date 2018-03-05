a1c494b4efaf3607ef33b52737ddced61b36864a
Chase <Chase@Chases-MBP.home>
2018-03-04 23:25:19 -0500

    Adding InterleavingStrings solution

---
+++ InterleavingStrings.java
@@ -0,0 +1,22 @@
+package questions;
+
+import java.util.List;
+
+public class InterleavingStrings {
+	@SuppressWarnings("unused")
+	private void helper(String str1, String str2, int index1, int index2, String result, List<String> results) {
+		if (index1 == str1.length() && index2 == str2.length()) {
+			results.add(new String(result));
+			return;
+		}
+		
+		if (index1 == str1.length()) {
+			helper(str1, str2, index1, index2 + 1, result + str2.charAt(index2), results);
+		} else if (index2 == str2.length()) {
+			helper(str1, str2, index1 + 1, index2, result + str1.charAt(index1), results);
+		} else {
+			helper(str1, str2, index1 + 1, index2, result + str1.charAt(index1), results);
+			helper(str1, str2, index1, index2 + 1, result + str2.charAt(index2), results);
+		}
+	}
+}
