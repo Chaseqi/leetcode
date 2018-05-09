64256388d197922e8308179a2d31a3b6d133a4e7
Chase <chaseqi0808@gmail.com>
2018-05-09 00:03:06 -0400

    bug fix

--- questions/PreorderAndPostOrderReconstructTree.java
+++ questions/PreorderAndPostOrderReconstructTree.java
@@ -5,13 +5,14 @@ package questions.leetcode.questions;
 // Time Complexity: T(n) = T(n - 1) + 1  --> O(n)
 public class PreorderAndPostOrderReconstructTree {
 	
-	public TreeNode reconstructTree(int[] preorder, int[] postorder) {
+	public static TreeNode reconstructTree(int[] preorder, int[] postorder) {
 		int n = preorder.length;
 		
-		return helper(preorder, 0, n, postorder, 0, n);
+		TreeNode root = helper(preorder, 0, n - 1, postorder, 0, n - 1);
+		return root;
 	}
 	
-	private TreeNode helper(int[] preorder, int left1, int right1, int[] postorder, int left2, int right2) {
+	private static TreeNode helper(int[] preorder, int left1, int right1, int[] postorder, int left2, int right2) {
 		if (left1 > right1) {
 			return null;
 		}
@@ -21,12 +22,22 @@ public class PreorderAndPostOrderReconstructTree {
 		}
 		
 		TreeNode root = new TreeNode(preorder[left1]);
-		root.left = helper(preorder, left1 + 1, right1, postorder, left2, right2 - 1);
+		int leftChild = preorder[left1 + 1];
+		int index = left2;
+		for (; index < right2; index++) {
+			if (postorder[index] == leftChild) {
+				break;
+			}
+		}
+		
+		int leftSize = index - left2 + 1;
+		root.left = helper(preorder, left1 + 1, left1 + leftSize, postorder, left2, index);
+		root.right = helper(preorder,  left1 + leftSize + 1, right1, postorder, index + 1, right2 - 1);
 		
 		return root;
 	}
 	
-	private class TreeNode {
+	private static class TreeNode {
 		int val;
 		TreeNode left;
 		TreeNode right;
