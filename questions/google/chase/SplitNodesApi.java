2e0ee5f6afcda45f4f6d5fd0fa1483bff5d6a91d
Chase <chaseqi0808@gmail.com>
2018-10-02 23:10:15 -0400

    .

---
+++ questions/google/chase/SplitNodesApi.java
@@ -0,0 +1,57 @@
+package questions.leetcode.questions.google.chase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplitNodesApi {
+
+	List<Node> nodes;
+	public SplitNodesApi() {
+		nodes = new ArrayList<>();
+	}
+	
+	/**
+	 * Adds a new Node.
+	 * @param node the new Node;
+	 */
+	public void addNode(Node node) {
+		
+	}
+	
+	/**
+	 * Splits the nodes into clusters
+	 * @return the clusters
+	 */
+	public List<List<Node>> split() {
+		return null;
+	}
+	
+	/**
+	 * Find the list of nodes on the boundary 
+	 * @param nodes
+	 * @return
+	 */
+	public List<Node> getEdges(List<Node> nodes) {
+		return null;
+	}
+	
+	/**
+	 * Checks if the boundaries intersect with each other
+	 * @param edge1
+	 * @param edge2
+	 * @return
+	 */
+	public boolean isIntersected(List<Node> boundary1, List<Node> boundary2) {
+		return true;
+	}
+	
+	public class Node {
+		int x;
+		int y;
+		
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
